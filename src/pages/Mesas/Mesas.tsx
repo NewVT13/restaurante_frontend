9cbd4bf60e8057c95ac16e4575e38ecf5b204d3b
Vinicius Tarouquela <viniciustmoura13@gmail.com>
2026-08-27 15:12:05 -0300

    adiciona criação da integração do pedido

--- src/pages/Mesas/Mesas.tsx
+++ src/pages/Mesas/Mesas.tsx
@@ -4,10 +4,18 @@ import style from "./Mesas.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { getDataLocalStorage } from "../../utils/getDataLocalStorage";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import stylesIndex from "../../index.module.css";
+import { useNavigate } from "react-router";
 
 const dados = getDataLocalStorage();
 
-type mesa = {
+type Mesa = {
   id: number;
   nome: string;
   lugares: number | null;
@@ -17,10 +25,44 @@ type mesa = {
 };
 
 function Mesas() {
-  const [mesas, setMesas] = useState<mesa[]>([]);
+  const navegate = useNavigate();
+  const [mesas, setMesas] = useState<Mesa[]>([]);
+  const [nomeCliente, setNomeCliente] = useState("");
+  const [modalAberto, setModalAberto] = useState(false);
+  const [mesaClicada, setMesaClicada] = useState<Mesa | null>(null);
+
+  function abrirModal(mesa: Mesa) {
+    setModalAberto(true);
+    setMesaClicada(mesa);
+  }
+  function fecharModal() {
+    setModalAberto(false);
+  }
+
+  async function criarPedido(e: React.SubmitEvent) {
+    try {
+      e.preventDefault();
+      await axios.post(
+        "http://localhost:8888/pedidos",
+        {
+          mesa_id: mesaClicada?.id,
+          nome_cliente: nomeCliente,
+          data: "2026-08-26",
+        },
+        {
+          headers: {
+            Authorization: "Bearen " + dados.token,
+          },
+        },
+      );
+      navegate("/pedido-itens");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
 
   async function buscarMesas() {
-    const response = await axios.get<mesa[]>("http://localhost:8888/mesas", {
+    const response = await axios.get<Mesa[]>("http://localhost:8888/mesas", {
       headers: {
         Authorization: "Bearen " + dados.token,
       },
@@ -57,16 +99,40 @@ function Mesas() {
 
       <div className={style.containerChairs}>
         {mesas.map((mesa) => (
-          <div key={mesa.id} className={style.chair}>
+          <div
+            key={mesa.id}
+            className={style.chair}
+            onClick={() => abrirModal(mesa)}
+          >
             <div className={style.chairHeader}>
               <span>{mesa.reservado ? "Ocupado" : "Livre"}</span>
               <GiWoodenChair />
             </div>
-            <h3>{mesa.nome}</h3>
+            <h3>Mesa {mesa.nome}</h3>
             <span>{mesa.lugares} lugares</span>
           </div>
         ))}
       </div>
+      <Dialog open={modalAberto} onClose={fecharModal} maxWidth="md">
+        <form onSubmit={criarPedido}>
+          <DialogTitle>Mesa {mesaClicada?.nome} </DialogTitle>
+          <DialogContent>
+            <p>Informe o nome do cliente para abrir o pedido</p>
+            <div className={stylesIndex.containerInput}>
+              <label>Nome do cliente</label>
+              <input
+                type="text"
+                value={nomeCliente}
+                onChange={(e) => setNomeCliente(e.target.value)}
+                required
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <button type="submit">Criar pedido</button>
+          </DialogActions>
+        </form>
+      </Dialog>
     </div>
   );
 }
